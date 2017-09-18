d591381270fc2fbd706dd1840591e08a3644bfa4
Gael Sonney <gael.sonney@siteminder.com>
2017-09-18 22:38:44 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// var Vue = require('vue');
+
+new Vue({
+  el: '#root',
+
+  data: {
+    message : 'youhou'
+  },
+
+  ready: function () {
+
+  },
+
+  methods: {
+    getPrice(){
+      return '30';
+    }
+  }
+});
