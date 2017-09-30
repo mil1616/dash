b607e3c363cf17ceb468da04afa4b635e04d590f
Gael Sonney <gael.sonney@siteminder.com>
2017-09-30 11:35:03 +1000

    Add connection to mysql

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-var express        = require('express'),
-    bodyParser     = require('body-parser'),
-    methodOverride = require('method-override'),
-    errorHandler   = require('errorhandler'),
-    morgan         = require('morgan')
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('errorhandler'),
+  morgan = require('morgan'),
+  _mysql = require('mysql');
 
 var app = module.exports = express();
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static(__dirname + '/'));
@@ -30,6 +33,30 @@ if ('production' == app.get('env')) {
 // app.get('/', routes.index);
 // app.all('/api/events', api.events);
 // app.all('/api/events/:eventId', api.event)
+
+
+var HOST = 'localhost';
+var PORT = 3309;
+var MYSQL_USER = 'siteminder';
+var MYSQL_PASS = 'siteminder';
+var DATABASE = 'nodedb';
+var TABLE = 'task';
+
+var mysql = _mysql.createConnection({
+    host: HOST,
+    port: PORT,
+    user: MYSQL_USER,
+    password: MYSQL_PASS,
+});
+
+mysql.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... \n\n");
+} else {
+    console.log("Error connecting database ... \n\n");
+}
+});
+
 
 app.listen(8090);
 console.log('Magic happens on port 8090...');
