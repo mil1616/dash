3d54244f8c53938e4c074c2918f90d1d28facc02
Gael Sonney <gael.sonney@siteminder.com>
2017-09-30 11:32:51 +1000

    Add mysql database schema

---
+++ sql/task.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `task` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
