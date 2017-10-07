625120bb9344243125679a4b3cdcf9947e40e90a
Gael Sonney <gael.sonney@siteminder.com>
2017-10-07 19:17:32 +1100

    Add create and delete task

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ new Vue({
     task: {
       title: ''
     },
-    tasks: []
+    tasks: [],
+    list: null
   },
 
   ready: function() {
@@ -17,10 +18,24 @@ new Vue({
 
   methods: {
     getTestMessage() {
-      return 'TestMessage';
+
     },
 
     fetchTasks: function() {
+      var vm = this;
+      this.$http.get('http://localhost:8090/tasks').then(function(response) {
+        vm.$set('tasks', response.data)
+      }, function(error) {
+        console.log(error.statusText);
+      });
+
+      // other possibility
+      // this.$http.get('http://localhost:8090/tasks', function(tasks) {
+      //    this.$set('tasks', tasks)
+      //  })
+    },
+
+    oldFfetchTasks: function() {
       var tasks = [{
           id: 1,
           title: 'put water in the iron'
@@ -34,6 +49,20 @@ new Vue({
     },
 
     addEvent: function() {
+      var vm = this;
+      if (this.task.title) {
+        this.$http.post('http://localhost:8090/tasks', this.task).then(function(response) {
+          vm.tasks.push(vm.task);
+          vm.task = {
+            title: ''
+          };
+        }, function(error) {
+          console.log(error.statusText);
+        });
+      }
+    },
+
+    oldAddEvent: function() {
       if (this.task.title) {
         this.tasks.push(this.task);
         this.task = {
@@ -42,7 +71,19 @@ new Vue({
       }
     },
 
-    deleteEvent: function(index) {
+    deleteEvent: function(index, taskId) {
+      // if(confirm("Are you sure you want to delete this event?")) {
+      // $remove is a Vue convenience method similar to splice
+      var vm = this;
+      this.$http.delete('http://localhost:8090/task', {id : taskId}).then(function(response) {
+        vm.tasks.splice(index, 1);
+      }, function(error) {
+        console.log(error.statusText);
+      });
+      // }
+    },
+
+    oldDeleteEvent: function(index) {
       // if(confirm("Are you sure you want to delete this event?")) {
       // $remove is a Vue convenience method similar to splice
       this.tasks.splice(index, 1);
